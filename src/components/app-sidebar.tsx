b8595d9526a3169931baacc931a9905c40e4dea9
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-06 20:57:40 +0200

    Update app-sidebar.tsx

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -13,9 +13,9 @@ const navItems = [
   { href: "/calendar", icon: Calendar, label: "Calendrier" },
   { href: "/mobility", icon: Car, label: "Mobilité" },
   { href: "/meetup", icon: Users, label: "Événements Meetup" },
-  { href="/facebook-groups", icon: Facebook, label: "Groupes Facebook" },
-  { href="/interactive-map", icon: Map, label: "Carte Interactive" },
-  { href="/help", icon: LifeBuoy, label: "Aide" },
+  { href: "/facebook-groups", icon: Facebook, label: "Groupes Facebook" },
+  { href: "/map", icon: Map, label: "Carte Interactive" },
+  { href: "/help", icon: LifeBuoy, label: "Aide" },
 ];
 
 
